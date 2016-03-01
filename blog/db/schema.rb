880cd9ec5140a6dfe55a5c55cc99c46b23f398d7
ASharma <akshatsharma46@gmail.com>
2016-02-29 21:00:09 -0800

    first upload

--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20160214045918) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
+    t.string   "pickup"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "author"
